e30e0c123e0e92f664ec9e4553290e5c903c1ec1
alfiyaali2601 <alfiyaali177@gmail.com>
2023-03-23 14:51:40 +0530

    script commit

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 window.onload = function(){
     if(localStorage){
-        document.getElementById('contactform').addEventListener('submit',function(){
+     document.getElementById('contactform').addEventListener('submit',function(){
             var name = document.getElementById('name').value;
             var mail = document.getElementById('mail').value;
             var phone = document.getElementById('phone').value;
             var date = document.getElementById('date').value;
             var time = document.getElementById('time').value;
-            localStorage.setItem('name',name); 
-            localStorage.setItem('mail',mail); 
-            localStorage.setItem('phone',phone); 
-            localStorage.setItem('date',date); 
-            localStorage.setItem('time',time); 
+            let myObj = {name, mail, phone,date,time};
+            
+            localStorage.setItem("myObj",myObj);
+            console.log(localStorage)
         });
     }
 }
